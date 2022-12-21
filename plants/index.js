24b14eba201b1919172a3ab1367174dffb7bc594
Nadezhda Shelepova <nadia344g@gmail.com>
2022-12-21 20:08:23 +0300

    feat: implement prices section

--- plants/index.js
+++ plants/index.js
@@ -1,39 +1,15 @@
-//Burger Menu
-function Burger(setting) {
-  let privates = {};
-  privates.setting = setting;
-  privates.navigation = document.querySelector(privates.setting.navigate);
-  if (privates.navigation === null) { return; }
-
-  privates.nav = {
-    "navMini": privates.navigation.querySelector(privates.setting.navMini),
-    "navList": privates.navigation.querySelector(privates.setting.navList),
-  }
-  privates.class_active = privates.setting.class_active;
-  privates.extend = privates.setting.extend.map((item) => document.querySelector(item));
-
-  //Methods
-  this.clickMiniMenu = () => {
-    privates.nav.navMini.classList.toggle(privates.class_active);
-    privates.nav.navList.classList.toggle(privates.class_active);
-    document.body.classList.toggle(privates.class_active);
-    privates.extend.forEach(element => {
-      element.classList.toggle(privates.class_active);
-    });
-  }
-
-  if (privates.nav.navMini !== null) {
-    privates.nav.navMini.addEventListener('click', () => {
-      this.clickMiniMenu();
-    });
-  }
+let selectCity = document.getElementById('selectCity');
+let infoCity = document.querySelectorAll('.contacts-info');
+if (selectCity) {
+  selectCity.addEventListener('change', changeSelect);
 }
 
-new Burger({
-  "navigate": "nav",
-  "navMini": ".nav__mini",
-  "navList": ".nav__list",
-  "class_active": "active-burger",
-  "extend": []
-})
-
+function changeSelect() {
+  for (let i = 0; i < infoCity.length; i++) {
+    if (infoCity[i].querySelector('.city').innerHTML === selectCity.value) {
+      infoCity[i].classList.add("active");
+    } else {
+      infoCity[i].classList.remove("active");
+    }
+  }
+}
