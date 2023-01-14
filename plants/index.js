e83b37a09ab4c8877fa68b7d902df997280b8321
Nadezhda Shelepova <nadia344g@gmail.com>
2023-01-14 17:16:48 +0300

    feat: implement burger menu

--- plants/index.js
+++ plants/index.js
@@ -1,1 +1,60 @@
-console.log("1. Вёрстка валидная +10\n2. Вёрстка семантическая +20\nВ коде странице присутствуют следующие элементы:\n - <header>, <main>, <footer> +3\n - пять элементов <section> +3\n - только один заголовок <h1> +3\n - четыре заголовка <h2> +3\n - один элемент <nav> (панель навигации) +3\n - два списка ul > li > a (панель навигации, ссылки на соцсети) +3\n - пять кнопок <button> +2\n3. Вёрстка соответствует макету +48\n - блок <header> +6\n - секция welcome +7\n - секция about +7\n - секция service +7\n - секция prices +7\n - секция contacts +7\n - блок <footer> +7\n4. Требования к css + 12\n - для построения сетки используются флексы или гриды +2\n - при уменьшении масштаба страницы браузера вёрстка размещается по центру, а не сдвигается в сторону +2\n - фоновый цвет тянется на всю ширину страницы +2\n - иконки добавлены в формате .svg +2\n - изображения добавлены в формате .jpg или .png +2\n - есть favicon +2\n5. Интерактивность, реализуемая через css +20\n - плавная прокрутка по якорям +5\n - иконки соцсетей в футере при нажатии на них ведут на гитхаб автора проекта и на страницу курса +5\n - интерактивность включает в себя не только изменение внешнего вида курсора, но и другие визуальные эффекты +5\n - обязательное требование к интерактивности: плавное изменение внешнего вида элемента при наведении и клике не влияющее на соседние элементы +5");
+console.log("1. Вёрстка соответствует макету. Ширина экрана 768px +24\n - блок <header> +2\n - секция welcome +3\n - секция about +4\n - секция service +4\n - секция prices +4\n - секция contacts +4\n - блок <footer> + 3\n2. Вёрстка соответствует макету. Ширина экрана 380px +24\n - блок <header> +2\n - секция welcome +3\n - секция about +4\n - секция service +4\n - секция prices +4\n - секция contacts +4\n - блок <footer> +3\n3. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15\n - нет полосы прокрутки при ширине страницы от 1440рх до 380px +7\n - нет полосы прокрутки при ширине страницы от 380px до 320рх +8\n4. На ширине экрана 380рх и меньше реализовано адаптивное меню +22\n - при ширине страницы 380рх панель навигации скрывается, появляется бургер-иконка +2\n - при нажатии на бургер-иконку плавно появляется адаптивное меню +4\n - адаптивное меню соответствует цветовой схеме макета +4\n - при нажатии на крестик адаптивное меню плавно скрывается уезжая за экран +4\n - ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +4\n - при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, также скрытие меню происходит если сделать клик вне данного окна +4");
+
+//Burger Menu
+class Burger {
+    constructor(setting) {
+        this.setting = setting;
+        this.navigation = document.querySelector(this.setting.navigate);
+        if (this.navigation === null) { return; }
+
+        this.nav = {
+            "navMini": this.navigation.querySelector(this.setting.navMini),
+            "navList": this.navigation.querySelector(this.setting.navList),
+        };
+        this.class_active = this.setting.class_active;
+
+        //Methods
+        this.clickMiniMenu = () => {
+            document.body.classList.toggle(this.class_active);
+            this.navigation.classList.toggle(this.class_active);
+        };
+
+        this.clickMenuList = (event) => {
+            if (event.target.tagName != "A")
+                return;
+            //Close burger
+            if (window.matchMedia(`(max-width: ${setting.break_point}px)`).matches) {
+                this.clickMiniMenu();
+            }
+        };
+
+        if (this.nav.navMini !== null) {
+            this.nav.navMini.addEventListener('click', () => {
+                this.clickMiniMenu();
+            });
+        }
+        if (this.nav.navList !== null) {
+            this.nav.navList.addEventListener('click', (event) => {
+                this.clickMenuList(event);
+            });
+        }
+        document.addEventListener('click', (event) => {
+            const target = event.target;
+            const its_menu = target == this.navigation || this.navigation.contains(target);
+            if (!its_menu && this.navigation.classList.contains("active-burger")) {
+                this.clickMiniMenu();
+            }
+        });
+    }
+}
+
+new Burger({
+    "navigate": ".nav",
+    "navMini": ".nav__mini",
+    "navList": ".nav__list",
+    "class_active": "active-burger",
+    "break_point": 599,
+})
+
+
+
