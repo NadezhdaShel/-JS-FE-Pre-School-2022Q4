4adfb654638fd40a29930b53737a222e07348acc
NadezhdaShel <nadia344g@gmail.com>
2023-02-20 19:58:37 +0300

    feat: implement writing settings to local storage

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -3,12 +3,12 @@ import './css/owfont-regular.css';
 import './css/style.css';
 
 import { setLocalStorage, getLocalStorage } from './js/personal.js';
+import addSettings from './js/settings';
 import showTime from './js/time.js';
 import setBg from './js/slider.js';
 import getQuotes from './js/quote.js';
 import addWeather from './js/weather.js';
 import audioPlayer from './js/audio.js';
-import addSettings from './js/settings';
 
 window.addEventListener('beforeunload', setLocalStorage);
 window.addEventListener('load', getLocalStorage);
--- momentum/src/js/personal.js
+++ momentum/src/js/personal.js
@@ -1,11 +1,48 @@
 const nameUser = document.querySelector('.name');
-const city = document.querySelector('.city');
+const city = document.querySelector('.city') || 'Minsk';
 const playList = document.querySelector('.play-list');
+let options = {
+    language: 'en',
+    photoSource: 'github',
+    blocks: ['time', 'date', 'greeting', 'quote', 'weather', 'audio']
+}
+
+function getOptions() {
+    const checkedLanguage = document.querySelector('input[name="language"]:checked');
+    const checkedBlocks = document.querySelectorAll('input[name="elements"]:checked');
+    let blocks = [];
+    checkedBlocks.forEach(elem => {
+        blocks.push(elem.value);
+    })
+    options.blocks = blocks;
+    options.language = checkedLanguage.value;
+}
+
+function setOptions() {
+    const languageSettings = document.querySelectorAll('input[name="language"]');
+    const blocksSettings = document.querySelectorAll('input[name="elements"]');
+    languageSettings.forEach(elem => {
+        if (elem.value === options.language) {
+            elem.checked = true;
+        } else {
+            elem.checked = false;
+        }
+    })
+    blocksSettings.forEach(elem => {
+        if (options.blocks.includes(elem.value)) {
+            elem.checked = true;
+        } else {
+            elem.checked = false;
+        }
+    })
+}
 
 function setLocalStorage() {
     localStorage.setItem('name', nameUser.value);
     localStorage.setItem('city', city.value);
     localStorage.setItem('playListHeight', playList.offsetHeight);
+    getOptions();
+    localStorage.setItem('options', JSON.stringify(options));
 }
 
 function getLocalStorage() {
@@ -18,6 +55,10 @@ function getLocalStorage() {
     if (localStorage.getItem('playListHeight')) {
         playList.style.height = localStorage.getItem('playListHeight') + 'px';
     }
+    if (localStorage.getItem('options')) {
+        options = JSON.parse(localStorage.getItem('options'));
+        setOptions();
+    }
 }
 
-export { setLocalStorage, getLocalStorage}
+export { setLocalStorage, getLocalStorage }
